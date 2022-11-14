49a8f749e07f15b3f728958583e993c1b1f54875
Veta Yurkina <vetagryff21@gmail.com>
2022-11-14 20:06:07 +0500

    Lesson 5

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,14 +1,42 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
 
     //IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public MusicPlayer() {}
+
+    public void setMusic(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
     public void playMusic() {
-        System.out.println("Play: "+ music.getSong());
+        System.out.println("Playlist:");
+        this.musicList.forEach(music -> System.out.println(music.getSong()));
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
     }
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,8 +9,15 @@ public class TestSpring {
 
 //        Music music = context.getBean("musicBean", Music.class);
 //        MusicPlayer player = new MusicPlayer(music);
+
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+        System.out.println("Player name is " + musicPlayer.getName());
+        System.out.println("Player volume is " + musicPlayer.getVolume());
+        System.out.println();
+
         musicPlayer.playMusic();
+
         context.close();
     }
 }
