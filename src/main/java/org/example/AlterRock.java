09ed757b93f41ea5f19c8ee17361bb9e05385101
Veta Yurkina <vetagryff21@gmail.com>
2022-11-16 18:39:20 +0500

    Lesson 8

--- src/main/java/org/example/AlterRock.java
+++ src/main/java/org/example/AlterRock.java
@@ -1,6 +1,13 @@
 package org.example;
 
 public class AlterRock implements Music{
+
+    private AlterRock(){}
+
+    public static AlterRock getAlterRock() {
+        return new AlterRock();
+    }
+
     @Override
     public String getSong() {
         return "LP - One More Light";
