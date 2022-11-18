d5f4d222b78a335d2d225f804d473e74734e2ea6
Veta Yurkina <vetagryff21@gmail.com>
2022-11-18 15:05:07 +0500

    lesson 13_1

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -1,11 +1,12 @@
 package org.example;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MySpringConfig.class);
         Computer computer = context.getBean("computer", Computer.class);
         System.out.println(computer);
 
